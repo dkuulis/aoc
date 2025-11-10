477b8a70fc0b257f656afe8ce19c10f7c19e369e
Davis Kulis <dkuulis@gmail.com>
2025-11-10 16:54:34 +0200

    2015 T20 solution

--- 2015/t20.py
+++ 2015/t20.py
@@ -1,26 +1,44 @@
+from itertools import count
 import lib
+
+def sum_factors(n):
+    result = 0
+    for x in range(1, int(n**0.5) + 1):
+        if n % x == 0:
+            r = n // x
+            result += x
+            result += r if r != x else 0
+    return result
+
+def sum_factors_w_limit(n, l):
+    result = 0
+    for x in range(1, int(n**0.5) + 1):
+        if n % x == 0:
+            r = n // x
+            result += x if r <= l else 0
+            result += r if x <= l and r != x else 0
+    return result
+
+def walk1(target):
+    for house in count(1):
+        presents = sum_factors(house) * 10
+        if presents >= target:
+            return house
+
+def walk2(target):
+    for house in count(1):
+        presents = sum_factors_w_limit(house, 50) * 11
+        if presents >= target:
+            return house
 
 def main():
     content = lib.read_content()
-    target = int(content) // 10
+    target = int(content)
 
-    house = 0
-    while True:
-        house += 1
-        presents = 0
-        for elve in range(1, house+1):
-            if house % elve == 0:
-                presents += elve
-        if presents >= target:
-            break
-        print(house, presents)
-        if house > 5041:
-            break
-
-    result1 = house
+    result1 = walk1(target)
     print(result1)
 
-    result2 = 0
+    result2 = walk2(target)
     print(result2)
 
 if __name__ == "__main__":
