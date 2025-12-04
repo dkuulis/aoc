a666fc522d0047596a19bf7c9728b52f48a01c66
Davis Kulis <dkuulis@gmail.com>
2025-12-04 23:13:51 +0200

    2025 boilerplate

---
+++ 2025/d10.py
@@ -0,0 +1,14 @@
+import re
+import lib
+
+def main():
+    lines = lib.read_lines()
+
+    result1 = 0
+    print(result1)
+
+    result2 = 0
+    print(result2)
+
+if __name__ == "__main__":
+    main()
