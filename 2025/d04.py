83c11eab3de6a3b42ea968b03c9c739029bdce7e
Davis Kulis <dkuulis@gmail.com>
2025-12-04 12:05:13 +0200

    2025 day 4 part 1

--- 2025/d04.py
+++ 2025/d04.py
@@ -1,12 +1,30 @@
 import lib
 
-decode = {"@": 1, ".": 0}
+def neighbours(x, y, size):
+    for dx in [-1, 0, 1]:
+        nx = x + dx
+        if nx >= 0 and nx < size:
+            for dy in [-1, 0, 1]:
+                if dx == 0 and dy == 0:
+                    continue
+                ny = y + dy
+                if ny >= 0 and ny < size:
+                    yield (nx, ny)
 
 def main():
     lines = lib.read_lines()
-    plan = [[decode[c] for c in line] for line in lines]
 
+    decode = {"@": 1, ".": 0}
+    map = [[decode[c] for c in line] for line in lines]
+
+    size = len(map)
     result1 = 0
+    for y in range(size):
+        for x in range(size):
+            if map[y][x] > 0:
+                n = sum(map[ny][nx] for nx, ny in neighbours(x, y, size))
+                result1 += 1 if n < 4 else 0
+
     print(result1)
 
     result2 = 0
