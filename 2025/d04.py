7abbe69a99202d1e9b41c1e9f8313c1447aeadd3
Davis Kulis <dkuulis@gmail.com>
2025-12-04 12:25:37 +0200

    2025 day 4 full

--- 2025/d04.py
+++ 2025/d04.py
@@ -11,23 +11,57 @@ def neighbours(x, y, size):
                 if ny >= 0 and ny < size:
                     yield (nx, ny)
 
+def check(map, x, y, size):
+    if map[y][x] > 0:
+        n = sum(map[ny][nx] for nx, ny in neighbours(x, y, size))
+        if n < 4:
+            yield x, y
+
+def accessible(map):
+    result = set()
+
+    size = len(map)
+    for y in range(size):
+        for x in range(size):
+            result.update(check(map, x, y, size))
+
+    return result
+
+def removable(map):
+
+    result = 0
+    size = len(map)
+
+    remove = accessible(map)
+    
+    while True:
+        result += len(remove)
+        
+        if len(remove) == 0:
+            break
+
+        next = set()
+        for x, y in remove:
+            map[y][x] = 0
+            next.update(neighbours(x, y, size))
+
+        remove = set()
+        for x, y in next:
+            remove.update(check(map, x, y, size))
+    # end while
+
+    return result
+
 def main():
     lines = lib.read_lines()
 
     decode = {"@": 1, ".": 0}
     map = [[decode[c] for c in line] for line in lines]
 
-    size = len(map)
-    result1 = 0
-    for y in range(size):
-        for x in range(size):
-            if map[y][x] > 0:
-                n = sum(map[ny][nx] for nx, ny in neighbours(x, y, size))
-                result1 += 1 if n < 4 else 0
-
+    result1 = len(accessible(map))
     print(result1)
 
-    result2 = 0
+    result2 = removable(map)
     print(result2)
 
 if __name__ == "__main__":
