0faa24c073d5b26545c442e0f77c1d9e4fe89429
Davis Kulis <dkuulis@gmail.com>
2025-12-05 09:01:27 +0200

    2025 day 5 part 1

--- 2025/d05.py
+++ 2025/d05.py
@@ -1,10 +1,15 @@
-import re
 import lib
 
 def main():
     lines = lib.read_lines()
+    split = next(i for i, e in enumerate(lines) if not e)
+
+    fresh = [tuple(map(int, line.split("-"))) for line in lines[:split]]
+    ingredients = [int(line) for line in lines[split+1:]]
 
     result1 = 0
+    for ingredient in ingredients:
+        result1 += 1 if  any(1 for r in fresh if r[0] <= ingredient and r[1] >= ingredient) else 0
     print(result1)
 
     result2 = 0
