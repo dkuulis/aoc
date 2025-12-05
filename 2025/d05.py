d56db012d4020b308ae29d75eaceaf54963ee1cf
Davis Kulis <dkuulis@gmail.com>
2025-12-05 10:46:23 +0100

    2025 day 5 full

--- 2025/d05.py
+++ 2025/d05.py
@@ -10,7 +10,19 @@ def main():
     result1 = sum(1 if any(1 for r in fresh if r[0] <= i and r[1] >= i) else 0 for i in ingredients)
     print(result1)
 
-    result2 = 0
+    fresh.sort(key = lambda x: x[0])
+    unified = []
+    start, end = 0, -1
+    for lo, hi in fresh:
+        if lo > end:
+            unified.append((start, end))
+            start = lo
+            end = hi
+        else:
+            end = max(end, hi)
+    unified.append((start, end))
+
+    result2 = sum(hi-lo+1 for lo, hi in unified)
     print(result2)
 
 if __name__ == "__main__":
