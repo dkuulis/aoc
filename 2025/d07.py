0850b429aedcc9fe424f38375bb0929afb95c269
Davis Kulis <dkuulis@gmail.com>
2025-12-08 08:52:04 +0200

    2025 day 7 cleanup

--- 2025/d07.py
+++ 2025/d07.py
@@ -1,27 +1,20 @@
-import re
 import lib
 
-def main():
-    lines = lib.read_lines()
-
-    decode = {".": 0, "^": 1}
-    beams = {i for i, c in enumerate(lines[0]) if c == "S"}
-    counts = {i:1 for i in beams}
-    splitters = [[decode[c] for c in line] for line in lines[1:]]
-
-    result1 = 0
+def part1(beams, splitters):
+    result = 0
     for row in splitters:
         next = set()
         for i in beams:
             if row[i] == 1:
                 next.add(i-1)
                 next.add(i+1)
-                result1 += 1
+                result += 1
             else:
                 next.add(i)
         beams = next
-    print(result1)
+    return result
 
+def part2(counts, splitters):
     for row in splitters:
         next = {}
         for i, c in counts.items():
@@ -31,7 +24,21 @@ def main():
             else:
                 next[i] = next.get(i, 0) + c
         counts = next
-    result2 = sum(c for c in counts.values())
+
+    return sum(c for c in counts.values())
+
+def main():
+    lines = lib.read_lines()
+
+    decode = {".": 0, "^": 1}
+    beams = {i for i, c in enumerate(lines[0]) if c == "S"}
+    counts = {i:1 for i in beams}
+    splitters = [[decode[c] for c in line] for line in lines[1:]]
+
+    result1 = part1(beams, splitters)
+    print(result1)
+
+    result2 =  part2(counts, splitters)
     print(result2)
 
 if __name__ == "__main__":
