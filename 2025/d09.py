64604092d664ad410c2aba95d9cc952f713127ec
Davis Kulis <dkuulis@gmail.com>
2025-12-09 11:40:09 +0200

    2025 day 9 part 2 errors

--- 2025/d09.py
+++ 2025/d09.py
@@ -1,8 +1,60 @@
 import lib
 
+def interval_range(i, j):
+    if i <= j:
+        return range(i, j+1)
+    else:
+        return range(i, j-1, -1) # handles descending order
+
 def area(p, q):
     return (p[0]-q[0]+1) * (p[1]-q[1]+1)
 
+def map_tiles(tiles):
+    index_x = sorted({t[0] for t in tiles})
+    map_x = {x:2*i+1 for i, x in enumerate(index_x)}
+    size_x = len(index_x)*2+1
+
+    index_y = sorted({t[1] for t in tiles})
+    map_y = {y:2*j+1 for j, y in enumerate(index_y)}
+    size_y = len(index_y)*2+1
+
+    mapped = [(map_x[t[0]], map_y[t[1]]) for t in tiles]
+
+    return size_x, size_y, mapped
+
+def outline_grid(size, size_x, size_y, mapped):
+    grid = [[2 for i in range(size_x)] for j in range(size_y)]
+    for i in range(size):
+        j = (i+1) % size
+        p1 = mapped[i]
+        p2 = mapped[j]
+        for x in interval_range(p1[0], p2[0]):
+            for y in interval_range(p1[1], p2[1]):
+                grid[y][x] = 1
+
+    return grid
+
+def fill(grid, sr, sc, orig, new):
+    rows, cols = len(grid), len(grid[0])
+
+    stack = [(sr, sc)]
+    while stack:
+        r, c = stack.pop()
+        if 0 <= r < rows and 0 <= c < cols and grid[r][c] == orig:
+            grid[r][c] = new
+            stack.append((r+1, c))
+            stack.append((r-1, c))
+            stack.append((r, c+1))
+            stack.append((r, c-1))
+
+def is_good(grid, mapped, i, j):
+    p1 = mapped[i]
+    p2 = mapped[j]
+    for x in interval_range(p1[0], p2[0]):
+        for y in interval_range(p1[1], p2[1]):
+            if grid[y][x] == 0:
+                return False
+    return True
 
 def main():
     lines = lib.read_lines()
@@ -12,7 +64,25 @@ def main():
     result1 = max(area(tiles[i], tiles[j]) for i in range(size-1) for j in range(i+1, size))
     print(result1)
 
-    result2 = 0
+    size_x, size_y, mapped = map_tiles(tiles)
+    grid = outline_grid(size, size_x, size_y, mapped)
+    fill(grid, 0, 0, 2, 0)
+
+    palette = [
+        [255, 0, 0],
+        [0, 0, 255],
+        [0, 255, 0],
+        [0, 255, 255]
+    ]
+
+    lib.save_bmp(grid, "output.bmp", palette)
+
+    areas = ((area(tiles[i], tiles[j]), i, j) for i in range(size-1) for j in range(i+1, size))
+    c = 0
+    for result2, i, j in sorted(areas, reverse=True, key = lambda x: x[0]):
+        c += 1
+        if is_good(grid, mapped, i, j):
+            break
     print(result2)
 
 if __name__ == "__main__":
--- 2025/lib.py
+++ 2025/lib.py
@@ -43,3 +43,47 @@ def min_max(gen):
         if val > max_val:
             max_val = val
     return min_val, max_val
+
+def save_bmp(data, filename, palette):
+    """
+    Save a 2D list of grayscale values (0-255) as a BMP image.
+    """
+    height = len(data)
+    width = len(data[0])
+
+    # Each pixel = 3 bytes (BGR)
+    row_size = (width * 3 + 3) & ~3  # pad to multiple of 4
+    pixel_array_size = row_size * height
+    file_size = 54 + pixel_array_size
+
+    with open(filename, "wb") as f:
+        # BMP Header
+        f.write(b"BM")
+        f.write(file_size.to_bytes(4, "little"))
+        f.write((0).to_bytes(4, "little"))  # reserved
+        f.write((54).to_bytes(4, "little")) # pixel data offset
+
+        # DIB Header (BITMAPINFOHEADER)
+        f.write((40).to_bytes(4, "little")) # header size
+        f.write(width.to_bytes(4, "little"))
+        f.write(height.to_bytes(4, "little"))
+        f.write((1).to_bytes(2, "little"))  # planes
+        f.write((24).to_bytes(2, "little")) # bits per pixel
+        f.write((0).to_bytes(4, "little"))  # compression
+        f.write(pixel_array_size.to_bytes(4, "little"))
+        f.write((2835).to_bytes(4, "little")) # horiz resolution
+        f.write((2835).to_bytes(4, "little")) # vert resolution
+        f.write((0).to_bytes(4, "little"))  # colors in palette
+        f.write((0).to_bytes(4, "little"))  # important colors
+
+        # Pixel data (bottom-up)
+        for row in reversed(data):
+            line = bytearray()
+            for val in row:
+                # grayscale → BGR triplet
+                bgr = palette[val]
+                line += bytes(bgr)
+            # pad row to multiple of 4
+            while len(line) < row_size:
+                line += b"\x00"
+            f.write(line)
