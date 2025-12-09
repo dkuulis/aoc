ee573df0fe95833131557bddaa6093c0121305f6
Davis Kulis <dkuulis@gmail.com>
2025-12-09 15:28:32 +0200

    2025 day 9 refresh

--- 2025/d09.py
+++ 2025/d09.py
@@ -9,62 +9,50 @@ def interval_range(i, j):
 def area(p, q):
     return (abs(p[0]-q[0])+1) * (abs(p[1]-q[1])+1)
 
-def map_tiles(tiles):
-    index_x = sorted({t[0] for t in tiles} | {t[0]+1 for t in tiles} | {-1})
-    map_x = {x:i for i, x in enumerate(index_x)}
-    size_x = len(index_x)
+def shrink_coordinate(tiles, accessor):
+    coordinates = {accessor(t) for t in tiles}
+    spaces = {c+1 for c in coordinates}
+    index = sorted(coordinates | spaces | {-1})
+    mapper = {x:i for i, x in enumerate(index)}
+    return mapper
 
-    index_y = sorted({t[1] for t in tiles} | {t[1]+1 for t in tiles} | {-1})
-    map_y = {y:j for j, y in enumerate(index_y)}
-    size_y = len(index_y)
+def shrink(tiles):
+    mapper_x = shrink_coordinate(tiles, lambda t: t[0])
+    mapper_y = shrink_coordinate(tiles, lambda t: t[1])
 
-    mapped = [(map_x[t[0]], map_y[t[1]]) for t in tiles]
+    shrunk = [(mapper_x[t[0]], mapper_y[t[1]]) for t in tiles]
+    return shrunk
 
-    return size_x, size_y, mapped
-
-def outline_grid(size, size_x, size_y, mapped):
-    grid = [[2 for _ in range(size_x)] for _ in range(size_y)]
+def outline(mapped):
+    size = len(mapped)
+    perimeter = set()
     for i in range(size):
         j = (i+1) % size
         p1 = mapped[i]
         p2 = mapped[j]
         for x in interval_range(p1[0], p2[0]):
             for y in interval_range(p1[1], p2[1]):
-                grid[y][x] = 1
+                perimeter.add((x,y))
+    return perimeter
 
-    return grid
+def is_good(p1, p2, perimeter):
+    x1, x2 = min(p1[0], p2[0]), max(p1[0], p2[0])
+    y1, y2 = min(p1[1], p2[1]), max(p1[1], p2[1])
 
-def fill(grid, sr, sc, orig, new):
-    rows, cols = len(grid), len(grid[0])
+    for p in perimeter:
+        if x1 < p[0] < x2 and y1 < p[1] < y2:
+            return False
 
-    stack = [(sr, sc)]
-    while stack:
-        r, c = stack.pop()
-        if 0 <= r < rows and 0 <= c < cols and grid[r][c] == orig:
-            grid[r][c] = new
-            stack.append((r+1, c))
-            stack.append((r-1, c))
-            stack.append((r, c+1))
-            stack.append((r, c-1))
-
-def is_good(grid, mapped, i, j):
-    p1 = mapped[i]
-    p2 = mapped[j]
-    for x in interval_range(p1[0], p2[0]):
-        for y in interval_range(p1[1], p2[1]):
-            if grid[y][x] == 0:
-                return False
     return True
 
 def part2(tiles, size):
-    size_x, size_y, mapped = map_tiles(tiles)
-    grid = outline_grid(size, size_x, size_y, mapped)
-    fill(grid, 0, 0, 2, 0)
+    small = shrink(tiles)
+    perimeter = outline(small)
 
     areas = ((area(tiles[i], tiles[j]), i, j) for i in range(size-1) for j in range(i+1, size))
 
     for result, i, j in sorted(areas, reverse=True, key = lambda x: x[0]):
-        if is_good(grid, mapped, i, j):
+        if is_good(small[i], small[j], perimeter):
             return result
 
 def main():
