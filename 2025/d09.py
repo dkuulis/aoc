e790316cb8b3cad48c88220368da692a51d5ecde
Davis Kulis <dkuulis@gmail.com>
2025-12-09 14:01:28 +0200

    2025 day 9 full

--- 2025/d09.py
+++ 2025/d09.py
@@ -7,23 +7,23 @@ def interval_range(i, j):
         return range(i, j-1, -1) # handles descending order
 
 def area(p, q):
-    return (p[0]-q[0]+1) * (p[1]-q[1]+1)
+    return (abs(p[0]-q[0])+1) * (abs(p[1]-q[1])+1)
 
 def map_tiles(tiles):
-    index_x = sorted({t[0] for t in tiles})
-    map_x = {x:2*i+1 for i, x in enumerate(index_x)}
-    size_x = len(index_x)*2+1
+    index_x = sorted({t[0] for t in tiles} | {t[0]+1 for t in tiles} | {-1})
+    map_x = {x:i for i, x in enumerate(index_x)}
+    size_x = len(index_x)
 
-    index_y = sorted({t[1] for t in tiles})
-    map_y = {y:2*j+1 for j, y in enumerate(index_y)}
-    size_y = len(index_y)*2+1
+    index_y = sorted({t[1] for t in tiles} | {t[1]+1 for t in tiles} | {-1})
+    map_y = {y:j for j, y in enumerate(index_y)}
+    size_y = len(index_y)
 
     mapped = [(map_x[t[0]], map_y[t[1]]) for t in tiles]
 
     return size_x, size_y, mapped
 
 def outline_grid(size, size_x, size_y, mapped):
-    grid = [[2 for i in range(size_x)] for j in range(size_y)]
+    grid = [[2 for _ in range(size_x)] for _ in range(size_y)]
     for i in range(size):
         j = (i+1) % size
         p1 = mapped[i]
@@ -56,6 +56,17 @@ def is_good(grid, mapped, i, j):
                 return False
     return True
 
+def part2(tiles, size):
+    size_x, size_y, mapped = map_tiles(tiles)
+    grid = outline_grid(size, size_x, size_y, mapped)
+    fill(grid, 0, 0, 2, 0)
+
+    areas = ((area(tiles[i], tiles[j]), i, j) for i in range(size-1) for j in range(i+1, size))
+
+    for result, i, j in sorted(areas, reverse=True, key = lambda x: x[0]):
+        if is_good(grid, mapped, i, j):
+            return result
+
 def main():
     lines = lib.read_lines()
     tiles = [tuple(map(int, line.split(","))) for line in lines]
@@ -64,25 +75,7 @@ def main():
     result1 = max(area(tiles[i], tiles[j]) for i in range(size-1) for j in range(i+1, size))
     print(result1)
 
-    size_x, size_y, mapped = map_tiles(tiles)
-    grid = outline_grid(size, size_x, size_y, mapped)
-    fill(grid, 0, 0, 2, 0)
-
-    palette = [
-        [255, 0, 0],
-        [0, 0, 255],
-        [0, 255, 0],
-        [0, 255, 255]
-    ]
-
-    lib.save_bmp(grid, "output.bmp", palette)
-
-    areas = ((area(tiles[i], tiles[j]), i, j) for i in range(size-1) for j in range(i+1, size))
-    c = 0
-    for result2, i, j in sorted(areas, reverse=True, key = lambda x: x[0]):
-        c += 1
-        if is_good(grid, mapped, i, j):
-            break
+    result2 = part2(tiles, size)
     print(result2)
 
 if __name__ == "__main__":
