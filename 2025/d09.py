d454f5f1f1af379725fa1f445e38fc602aa9d206
Davis Kulis <dkuulis@gmail.com>
2025-12-09 19:19:05 +0200

    2025 day 9 optimize

--- 2025/d09.py
+++ 2025/d09.py
@@ -1,11 +1,5 @@
 import lib
 
-def interval_range(i, j):
-    if i <= j:
-        return range(i, j+1)
-    else:
-        return range(i, j-1, -1) # handles descending order
-    
 def circular(l):
     size = len(l)
     for i in range(size):
@@ -15,16 +9,19 @@ def circular(l):
 def area(p, q):
     return (abs(p[0]-q[0])+1) * (abs(p[1]-q[1])+1)
 
-def collect_outline(tiles):
-    return {(x,y) for p1, p2 in circular(tiles) for x in interval_range(p1[0], p2[0])  for y in interval_range(p1[1], p2[1])}
+def is_good(p1, p2, tiles):
+    # min/max coordinates
+    px1, px2 = min(p1[0], p2[0]), max(p1[0], p2[0])
+    py1, py2 = min(p1[1], p2[1]), max(p1[1], p2[1])
 
-def is_good(p1, p2, perimeter):
-    x1, x2 = min(p1[0], p2[0]), max(p1[0], p2[0])
-    y1, y2 = min(p1[1], p2[1]), max(p1[1], p2[1])
+    for q1, q2 in circular(tiles):
+        # min/max coordinates
+        qx1, qx2 = min(q1[0], q2[0]), max(q1[0], q2[0])
+        qy1, qy2 = min(q1[1], q2[1]), max(q1[1], q2[1])
 
-    for p in perimeter:
-        if x1 < p[0] < x2 and y1 < p[1] < y2:
-            return False
+        # Two rectangles do not intersect if one is completely to the left, right, above, or below the other
+        if not(px2 <= qx1 or qx2 <= px1 or py2 <= qy1 or qy2 <= py1):
+            return False # part of outline is inside
 
     return True
 
@@ -35,11 +32,9 @@ def part1(tiles):
 
 def part2(tiles):
     size = len(tiles)
-    outline = collect_outline(tiles)
-
     areas = ((area(tiles[i], tiles[j]), i, j) for i in range(size-1) for j in range(i+1, size))
     for result, i, j in sorted(areas, reverse=True, key = lambda x: x[0]):
-        if is_good(tiles[i], tiles[j], outline):
+        if is_good(tiles[i], tiles[j], tiles):
             return result
 
 def main():
@@ -51,7 +46,6 @@ def main():
 
     result2 = part2(tiles)
     print(result2)
-
 
 if __name__ == "__main__":
     main()
