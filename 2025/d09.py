48b540599e25cc6912b510f9865d1b18392bf0e2
Davis Kulis <dkuulis@gmail.com>
2025-12-09 08:28:18 +0200

    2025 day 9 part 1

--- 2025/d09.py
+++ 2025/d09.py
@@ -1,10 +1,15 @@
-import re
 import lib
+
+def area(p, q):
+    return (p[0]-q[0]+1) * (p[1]-q[1]+1)
+
 
 def main():
     lines = lib.read_lines()
+    tiles = [tuple(map(int, line.split(","))) for line in lines]
+    size = len(tiles)
 
-    result1 = 0
+    result1 = max(area(tiles[i], tiles[j]) for i in range(size-1) for j in range(i+1, size))
     print(result1)
 
     result2 = 0
