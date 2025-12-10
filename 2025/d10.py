90ad0bec6507d8b35927aef83949c7ee2db1b2ba
Davis Kulis <dkuulis@gmail.com>
2025-12-10 20:53:31 +0200

    2025 day 10 part 2 errors

--- 2025/d10.py
+++ 2025/d10.py
@@ -1,11 +1,18 @@
 import re
 import lib
+import sys
+import operator
 from collections import namedtuple
 from itertools import combinations
 from functools import reduce
-import operator
 
 Machine = namedtuple('Machine', ["lights", "buttons", "joltages"])
+
+def iterate_with_rest(l):
+    for i in range(len(l)):
+        chosen = l[i]
+        rest = l[:i] + l[i+1:]
+        yield chosen, rest
 
 def parse(line):
 
@@ -24,17 +31,47 @@ def parse(line):
 
     return Machine(lights, buttons, joltages)
 
-def choose_buttons(machine):
+def choose_buttons(machine: Machine):
     for r in range(len(machine.buttons) + 1):
         for toggles in combinations(machine.buttons, r):
             if machine.lights == reduce(operator.xor, toggles, set()):
                 return r
 
-def choose_joltages(machine):
-    return 0
+def count_pushes(buttons, joltages, best, pressed):
+    for button, rest in iterate_with_rest(buttons):
+
+        button_pushes = min(joltages[counter] for counter in button)
+        if button_pushes == 0: # buton cannot be pushed
+            continue
+
+        # new remaining joltages
+        remaining_js = list(joltages)
+        for counter in button:
+            remaining_js[counter] -= button_pushes
+
+        max_needed_j = max(remaining_js)
+        if button_pushes + max_needed_j >= best: # cannot improve
+            continue
+
+        result = pressed | {str(button): button_pushes}
+        if max_needed_j > 0: # more push needed
+            button_pushes += count_pushes(rest, remaining_js, best - button_pushes, result)
+        else:
+            print(result) 
+
+        if button_pushes < best:
+            best = button_pushes
+
+    return best
+
+def choose_joltages(machine: Machine):
+    print("----")
+    descending = sorted(machine.buttons, reverse=True, key=lambda b: len(b))
+    remaining = list(machine.joltages)
+    return count_pushes(descending, remaining, sys.maxsize, {})
 
 def main():
-    lines = lib.read_lines()
+    lines = lib.read_lines("init")
     machines = [parse(line) for line in lines]
 
     result1 = sum(choose_buttons(machine) for machine in machines)
