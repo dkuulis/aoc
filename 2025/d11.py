afe332e16016f5a6571af253c73ce7ad9ee2d993
Davis Kulis <dkuulis@gmail.com>
2025-12-11 11:45:38 +0200

    2025 day 11 full

--- 2025/d11.py
+++ 2025/d11.py
@@ -1,3 +1,4 @@
+from collections import defaultdict, deque
 import lib
 
 def count_paths(graph, start, end, path):
@@ -12,14 +13,56 @@ def count_paths(graph, start, end, path):
 
     return total
 
+
+def topological_sort(graph):
+    inputs = defaultdict(int)
+
+    for u in graph:
+        for v in graph[u]:
+            inputs[v] += 1
+
+    queue = deque(u for u in graph if inputs[u] == 0)
+    order = []
+
+    while queue:
+        u = queue.popleft()
+        order.append(u)
+
+        for v in graph[u]:
+            inputs[v] -= 1
+            if inputs[v] == 0:
+                queue.append(v)
+
+    return order
+
+def count_paths_dp(graph, source, destination):
+
+    counts = defaultdict(int)
+    counts[source] = 1
+
+    for u in topological_sort(graph):
+        for v in graph[u]:
+            counts[v] += counts[u]
+
+    return counts[destination]
+
 def main():
     lines = lib.read_lines()
     connections = {key: value.split() for key, value in (line.split(":", 1) for line in lines)}
+    graph = defaultdict(list, connections)
 
-    result1 = count_paths(connections, "you", "out", [])
+    result1 = count_paths(graph, "you", "out", [])
     print(result1)
 
-    result2 = 0
+    a1 = count_paths_dp(graph, "svr", "dac")
+    a2 = count_paths_dp(graph, "dac", "fft")
+    a3 = count_paths_dp(graph, "fft", "out")
+
+    b1 = count_paths_dp(graph, "svr", "fft")
+    b2 = count_paths_dp(graph, "fft", "dac")
+    b3 = count_paths_dp(graph, "dac", "out")
+
+    result2 = a1*a2*a3 + b1*b2*b3
     print(result2)
 
 if __name__ == "__main__":
