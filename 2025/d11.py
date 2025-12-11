5aaaed8a29b4c41c2fde08532043c7aa1487aa27
Davis Kulis <dkuulis@gmail.com>
2025-12-11 07:40:37 +0200

    2025 day 11 part 1

--- 2025/d11.py
+++ 2025/d11.py
@@ -1,10 +1,22 @@
-import re
 import lib
+
+def count_paths(graph, start, end, path):
+    path = path + [start]
+
+    if start == end:
+        return 1
+
+    total = 0
+    for node in graph[start]:
+        total += count_paths(graph, node, end, path)
+
+    return total
 
 def main():
     lines = lib.read_lines()
+    connections = {key: value.split() for key, value in (line.split(":", 1) for line in lines)}
 
-    result1 = 0
+    result1 = count_paths(connections, "you", "out", [])
     print(result1)
 
     result2 = 0
