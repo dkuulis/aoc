3745531820a5db504e2ae56e59eaf1391de4b44f
Davis Kulis <dkuulis@gmail.com>
2025-12-12 08:35:15 +0200

    2025 day 12 parse

--- 2025/d12.py
+++ 2025/d12.py
@@ -1,8 +1,42 @@
 import re
 import lib
 
+def parse(lines):
+    size = len(lines)
+    pattern = r"((?P<index>\d+):)|((?P<x>\d+)x(?P<y>\d+):(?P<shapes>[\d ]+))"
+    decode = {"#":1, ".":0}
+
+    tiles = []
+    grids = []
+
+    i = 0
+    while i < size:
+        m = re.match(pattern, lines[i])
+
+        # tile definition
+        if m["index"]:
+            shape = []
+            i += 1
+            while i < size and lines[i]:
+                shape.append(lines[i])
+                i += 1
+            tiles.append([[decode[c] for c in l] for l in shape])
+
+        # grid specifications
+        if m["shapes"]:
+            x = int(m["x"])
+            y = int(m["y"])
+            indexes = list(map(int, m["shapes"].split()))
+            grids.append(((x,y),indexes))
+
+        # advance
+        i += 1
+
+    return tiles, grids
+
 def main():
-    lines = lib.read_lines()
+    lines = lib.read_lines("init")
+    tiles, grids = parse(lines)
 
     result1 = 0
     print(result1)
